a5335487e2ec9ac3070f1bfd303aed9f2fe2c687
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2022-12-08 17:37:21 -0300

    select cliente cartao

--- src/Pages/Cartao/Cartao.js
+++ src/Pages/Cartao/Cartao.js
@@ -1,71 +1,33 @@
-import React, {useState} from "react";
-import "./Cartao.css";
-import Cards from 'react-credit-cards';
-import 'react-credit-cards/es/styles-compiled.css';
-import { useForm } from "react-hook-form";
-import Box from '@mui/material/Box';
+import React, { useState, useEffect, useContext } from "react";
+import FormCartao from "../../Components/FormCartao/FormCartao";
+import MyContext from '../../context/myContext';
+
+import FormControl from '@mui/material/FormControl';
+import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 
 const Cartao = () => {
-    const [card, setCard] = useState({
-        cvc: '',
-        expiry: '',
-        focus: '',
-        name: '',
-        number: '',
-    });
+    const { clientes, setClientes } = useContext(MyContext);
 
-    const handleInputFocus = (e) => {
-        setCard({...card, focus: e.target.name});
-    }
 
-    const handleInputChange = (e) => {
-        const {name, value} = e.target;
-        setCard({...card, [name]: value});
-    }
-
-    const { register, handleSubmit, reset,  errors } = useForm();
-
-    const onSubmit = data => {
-        console.log(data)
-        reset();
-    }
-
-    const locale = {
-        valid: 'validade',
-        month: 'mês',
-        year: 'ano',
-        cvc: 'cvc',
-        name: 'nome',
-        number: 'número do cartão',
-        
-    };
-
-    return (
-        <div className="cartao">
-            <Cards
-            locale={locale}
-            cvc={card.cvc}
-            expiry={card.expiry}
-            focused={card.focus}
-            name={card.name}
-            number={card.number}
-            />
-            <Box className="container-form-card" onSubmit={handleSubmit(onSubmit)} component="form"  noValidate autoComplete="off" > 
-                <TextField type='tel' {...register('number')} id="number" label="Número do Cartão" variant="standard" onChange={handleInputChange} onFocus={handleInputFocus} />
-                <TextField type='text' {...register('name')} id="name" label="Nome do Titular" variant="standard" onChange={handleInputChange} onFocus={handleInputFocus} />
-                <Box>
-                    <TextField type='tel' {...register('expiry')} id="expiry" label="Validade" variant="standard" onChange={handleInputChange} onFocus={handleInputFocus} />
-                <TextField type='tel' {...register('cvc')} id="cvc" label="CVC" variant="standard" onChange={handleInputChange} onFocus={handleInputFocus} />
-                </Box>
-                
-                <Button type='submit' variant="contained" color="primary">finalizar</Button>
-            </Box>
-        </div>
-               
-
-    );
+ 
+    return(
+        <>
+        <FormControl sx={{marginTop: 10}} fullWidth>                    
+        <Autocomplete
+            className=""
+            id="nome-clientes"
+            name="clientes"                            
+            options={clientes.name}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField label="Clientes" />}
+        />
+        </FormControl>
+        <FormCartao />
+        </>
+    )
 }
+
+
 
 export default Cartao;
